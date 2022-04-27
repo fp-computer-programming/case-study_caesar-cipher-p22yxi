dd00c5c9ca5d770eb0d7b96b4fe1d3bba99d9d24
p22yxi <p22yxi@fairfieldprep.org>
2022-04-27 10:09:45 -0400

    ceasar_cipher_try

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,9 @@
-
-
 # Imports
 from string import ascii_uppercase
 
+# Main
+user_file = input("Please enter a file to be encrypted: ")
+shift_value = input("Please enter a shift value: ")
 
 # Functions
 def cipher_key(shift):
@@ -11,20 +12,33 @@ def cipher_key(shift):
 
     return dict(zip(original_letters, shifted_letters))
 
+with open(user_file, 'r') as file1:
+    lines = file1.readlines()
+    for x in lines:
+        line = x
+
 
 def shift_line(line, dict_key):
-    new_line = ""
+    new_line = " "
+    for w in line:
+        for (k, v) in dict_key.items():
+            if k == w:
+                new_line += w
+        
     # Add code here
     return new_line
 
 
 def encrypt_message(filename, dict_key):
+    ofile = open(filename, "w")
+    ofile.write(new_line + "\n")
+    ofile.close()
+
+
     # Add code here
 
 
-# Main
-user_file = input("Please enter a file to be encrypted: ")
-shift_value = input("Please enter a shift value: ")
+
 
 key = cipher_key(shift_value)
 
